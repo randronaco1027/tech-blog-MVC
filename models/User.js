65850e67a56c2bdc74494b1d548e3f75f14d6617
randronaco <randronaco1027@gmail.com>
2022-05-30 23:47:18 -0400

    built out models

---
+++ models/User.js
@@ -0,0 +1,49 @@
+const { Model, DataTypes } = require('sequelize')
+const sequelize = require('../config/connection')
+const bcrypt = require('bcrypt')
+
+class User extends Model {
+    checkPassword(userPassword) {
+        return bcrypt.compareSync(userPassword, this.password)
+    }
+}x
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    },
+    {
+        hooks: {
+            async beforeCreate(newData) {
+                newData.password = await bcrypt.hash(newData.password, 10)
+                return newData
+            },
+
+            async beforeUpdate(updatedData) {
+                updatedData.password = await bcrypt.hash(updatedData.password, 10)
+                return updatedData
+            }
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user'
+    }
+)
+
+module.exports = User
