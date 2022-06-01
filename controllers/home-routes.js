2cab13ea250380691624e99dbc7316a8a55dbcb8
randronaco <randronaco1027@gmail.com>
2022-06-01 13:42:15 -0400

    established basic home-routes api -- WIP

---
+++ controllers/home-routes.js
@@ -0,0 +1,45 @@
+const router = require('express').Router()
+const sequelize = require('../config/connection')
+const { Post, User, Comment } = require('../models')
+
+router.get('/', (req, res) => {
+    Post.findAll({
+        attributes: ['id', 'title', 'post_text'],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id'],
+                include: {
+                    model: User,
+                    attributes: 'username'
+                }
+            },
+            {
+                model: User,
+                attributes: 'username'
+            }
+        ]
+    })
+    .then(postData => {
+        const posts = postData.map(post => post.get({ plain: true }))
+        res.render('homepage', {
+            posts,
+            loggedIn: req.session.loggedIn
+        })
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json(err)
+    })
+})
+
+router.get('/login', (req, res) => {
+    if (req.session.logged) {
+        res.redirect('/')
+        return
+    }
+
+    res.render('login')
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
-
+const exphbs = require('express-handlebars')
 
 const app = express()
 const PORT = process.env.PORT || 3001
@@ -21,6 +21,9 @@ const sess = {
 
 app.use(session(sess))
 
+const hbs = exphbs.create({ helpers })
+app.engine('handlebars', hbs.engine)
+app.set('view engine', 'handlebars')
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
