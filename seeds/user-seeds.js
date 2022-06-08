93d8ba956d5b1aae57015be73e03faf89d8ebe2e
randronaco <randronaco1027@gmail.com>
2022-06-07 23:10:14 -0400

    fixed user model

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -3,18 +3,22 @@ const { User } = require('../models')
 const userData = [
     {
         username: 'user1',
+        email: 'email1@com',
         password: 'password1'
     },
     {
         username: 'user2',
+        email: 'email2@com',
         password: 'password2'
     },
     {
         username: 'user3',
+        email: 'email3@com',
         password: 'password3'
     },
     {
         username: 'user4',
+        email: 'email4@com',
         password: 'password4'
     },
 ]
